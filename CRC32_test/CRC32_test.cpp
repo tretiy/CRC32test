2c7b84efc4af06e55a78bc4094d190a16fb9b603
Dmitry Tretyakov <dmitry.tretyakov@c-map.com>
2018-07-13 10:25:16 +0300

    Add project files.

---
+++ CRC32_test/CRC32_test.cpp
@@ -0,0 +1,12 @@
+﻿// CRC32_test.cpp : Defines the entry point for the application.
+//
+
+#include "CRC32_test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
